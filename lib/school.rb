ccfbdb216103d1d413bc7de58072c32d12c12020
Chance Smith <chancesmith97@hotmail.com>
2019-03-01 04:53:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  attr_reader :name_of_school, :roster
+  def initialize(name_of_school)
+    @name_of_school = name_of_school
+    @roster = {}
+    
+  end
+  
+  def add_student(name, grade)
+    self.roster[grade] ||=[]
+    self.roster[grade] << name
+    
+  end
+    
+    
+end
